30a7b23e0116dc927bda7b4b1144aa5482d7ca25
Polyane Tuag <pollyportugues@hotmail.com>
2021-02-13 17:47:41 -0300

    Rotas  http criadas

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,36 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message:'Hello World'});
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+  ]);
+})
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Project 1',
+    'Project 2',
+    'Project 3',
+  ])
+})
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 4',
+    'Project 2',
+    'Project 3',
+  ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Project 4',
+    'Project 2',
+  ])
 })
 
 app.listen(5000, () => {
