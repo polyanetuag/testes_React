a8917ecccccd96fb35561ce47f876b76c93641f9
Polyane Tuag <pollyportugues@hotmail.com>
2021-03-14 18:32:39 -0300

    Criando componentes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Header />
+    </div> 
+  )
+}
+
+export default App;
